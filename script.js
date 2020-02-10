31e94eba869ddd5b6a4f1ae68fb3d6881aa9fcde
vfigu <vicente.figueroa.950@my.csun.edu>
2020-02-10 00:23:00 -0800

    Integrates datatable functionality for adding rows with the Bluetooth scanner, and implements a data file exporter.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const app = {
     start() {
         $(document).ready(function () {
-            $('#table-output').DataTable();
+            $('#table-output').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ]
+            });
         });
     }
 };
 
 $(document).ready(() => app.start());
+
+function insertDataRow(deviceId, name, rssi, timeStamp) {
+    $(document).ready(function() {
+        var t = $('#table-output').DataTable();
+
+        $('#addRow').on( 'click', function () {
+        t.row.add([deviceId, name, rssi, timeStamp]).draw(false);
+    });
+})}
