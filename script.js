c2c5c161a551f94b4b612341fbdde45361ecbe1e
vfigu <vicente.figueroa.950@my.csun.edu>
2020-02-11 06:43:39 -0800

    Export data options fix.

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const app = {
     start() {
         $(document).ready(function () {
-            let table = $('#table-output').DataTable({
-                dom: 'Bfrtip',
-                lengthMenu: [
-                    [ 10, 25, 50, -1 ],
-                    [ '10 rows', '25 rows', '50 rows', 'Show all' ]
-                ],
-                buttons: [
-                    'pageLength', 'copy', 'csv', 'excel', 'pdf', 'print'
-                ]
-            });
+            let table = $('#table-output').DataTable({});
 
             function createTable(){
                 table.destroy();
                 table = $('#table-output').DataTable({
+                    dom: 'Bfrtip',
+                    lengthMenu: [
+                        [ 10, 25, 50, -1 ],
+                        [ '10 rows', '25 rows', '50 rows', 'Show all' ]
+                    ],
+                    buttons: [
+                        'pageLength', 'copy', 'csv', 'excel', 'pdf', 'print'
+                    ],
                     data : OutputData,
                     "columns" : [
                         { "data" : "Device ID" },
@@ -25,6 +24,9 @@ const app = {
                 });
             }
             $('#stop-button').on( 'click', function () {
+                createTable();
+            });
+            $('#create-table').on( 'click', function () {
                 createTable();
             });
             $('#destroy-table').on( 'click', function () {
